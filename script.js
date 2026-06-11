ac366c53d460770a771a3455495c2973ceea9c28
falgoweb <agokity@gmail.com>
2026-06-11 09:34:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -741,39 +741,4 @@ async function exportPDF(){
     22
   );
 
-  doc.autoTable({
-  html: "#gridTable",
-  startY: 30,
-  theme: "grid"
-
-  styles: {
-    fontSize: 14,
-    cellPadding: 4,
-    halign: "center",
-    valign: "middle",
-    textColor: [0, 0, 0],
-    fontStyle: "bold".
-     lineColor: [0, 0, 0],
-  },
-
-  headStyles: {
-    fillColor: [255,255,255],
-    textColor: [0,0,0],
-    fontStyle: "bold"
-  },
-
-  didParseCell: function(data) {
-    data.cell.styles.lineWidth = 0.3;
-    data.cell.styles.textColor = [0, 0, 0];
-  },
-
-  columnStyles: {
-    0: { halign: "center" },
-    1: { halign: "center" },
-    2: { halign: "center" },
-    3: { halign: "center" },
-  },
-});
-
-  doc.save("GridFal.pdf");
-},
+ 
