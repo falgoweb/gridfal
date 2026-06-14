573f79e5e5929944bce072c63b86d71a9169225d
falgoweb <agokity@gmail.com>
2026-06-14 21:21:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -916,7 +916,7 @@ doc.setFontSize(9);
   fillColor: [255,255,255],
   textColor: [0,0,0],
   fontStyle: "bold",
-  fontSize: 21,
+  fontSize: 12,
   halign: "center",
   valign: "middle",
   lineColor: [0,0,0],
@@ -941,19 +941,7 @@ doc.setFontSize(9);
   lineColor: [80,80,80],
   lineWidth: 0.4,
 },
-   didParseCell: function(data) {
-
-  if(data.section === "head"){
-    data.cell.styles.lineColor = [0,0,0];
-    data.cell.styles.lineWidth = 1;
-  }
-
-  if(data.section === "body"){
-    data.cell.styles.lineColor = [80,80,80];
-    data.cell.styles.lineWidth = 0.4;
-  }
-
-   }
+   
 });
 
 // Total data
